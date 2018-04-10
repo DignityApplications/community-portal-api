c767d29fba355c4ee9dca98e828dca1974b2ebc7
Unknown <dignityapps@gmail.com>
2018-04-09 21:50:52 -0400

    heroku uses 'production' environment

--- knexfile.js
+++ knexfile.js
@@ -25,7 +25,7 @@ module.exports = {
     }
   },
 
-  heroku: {
+  production: {
     client: 'pg',
     connection: 'postgres://bgnverwizackay:14dced267ac33a704665b2011cd1bdf517aea943140eaeaa2599da381e3386f7@ec2-54-221-192-231.compute-1.amazonaws.com:5432/d9t7jas6pehll3',
     migrations: {
