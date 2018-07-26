81f7726bb097efbc86d347ea49cce0ba13b2c856
Unknown <dignityapps@gmail.com>
2018-07-25 20:53:46 -0400

    fix for foreign key constraint

--- src/server/db/seeds/heroku/08_event_reservations.js
+++ src/server/db/seeds/heroku/08_event_reservations.js
@@ -6,21 +6,21 @@ exports.seed = function(knex, Promise) {
     
     .then(() => {
         return knex('event_reservations').insert({
-            user_id: 15, // Member 3
+            user_id: 5, // Member 3
             event_id: 1, // Knitting Club
             attendees: 2
         });
     })
     .then(() => {
         return knex('event_reservations').insert({
-            user_id: 16, // Member 4
+            user_id: 6, // Member 4
             event_id: 2, // Concerto
             attendees: 5
         });
     })
     .then(() => {
         return knex('event_reservations').insert({
-            user_id: 17, // Member 5
+            user_id: 7, // Member 5
             event_id: 3, // Board Meeting
             attendees: 1
         });
